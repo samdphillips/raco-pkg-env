00dbe71ef59599c74691ac0504b8f42be1352813
Leif Andersen <leif@leifandersen.net>
2022-08-26 20:47:16 -0400

    Update the PS1 environment variable in the activate script

--- raco-pkg-env-lib/info.rkt
+++ raco-pkg-env-lib/info.rkt
@@ -3,7 +3,7 @@
 (define name "raco-pkg-env-lib")
 (define collection "raco-pkg-env")
 (define version "0.1.2")
-(define deps '("base"))
+(define deps '("base" "at-exp-lib"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
 (define raco-commands
