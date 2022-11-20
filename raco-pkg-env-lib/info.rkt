004da1db87dc69cf8dfe14403d5f2f6efe0abe02
Sam Phillips <samdphillips@gmail.com>
2022-11-19 21:20:55 -0800

    Bump version.  Add license metadata.  Closes #14.

--- raco-pkg-env-lib/info.rkt
+++ raco-pkg-env-lib/info.rkt
@@ -2,9 +2,12 @@
 
 (define name "raco-pkg-env-lib")
 (define collection "raco-pkg-env")
-(define version "0.1.3")
+(define version "0.1.4")
 (define deps '("base" "at-exp-lib"))
 (define build-deps '())
 (define pkg-authors '(samdphillips@gmail.com))
+(define license 'Apache-2.0)
 (define raco-commands
-  '(("pkg-env" (submod raco-pkg-env main) "sets up a package environment" 10)))
+  '(("pkg-env" (submod raco-pkg-env main)
+               "sets up a package environment"
+               10)))
--- raco-pkg-env/info.rkt
+++ raco-pkg-env/info.rkt
@@ -1,15 +1,11 @@
 #lang info
 
 (define name "raco-pkg-env")
-(define version "0.1.3")
+(define version "0.1.4")
 (define collection "raco-pkg-env")
+(define deps '("base" "raco-pkg-env-lib"))
+(define implies '("raco-pkg-env-lib"))
+(define build-deps '("scribble-lib"))
+(define scribblings '(["scribblings/raco-pkg-env.scrbl"]))
 (define pkg-authors '(samdphillips@gmail.com))
-(define deps
-  '("base"
-    "raco-pkg-env-lib"))
-(define implies
-  '("raco-pkg-env-lib"))
-(define build-deps
-  '("scribble-lib"))
-(define scribblings
-  '(["scribblings/raco-pkg-env.scrbl"]))
+(define license 'Apache-2.0)
